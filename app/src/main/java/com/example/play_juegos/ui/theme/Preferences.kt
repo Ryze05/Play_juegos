15b9244b8c7b668fe53842a3b51998e331c8f60e
ROMERO LLUMIQUINGA <romromllu@a1-16.infor.caminas>
2025-09-30 16:55:19 +0200

    progress bar y filterchip

--- app/src/main/java/com/example/play_juegos/ui/theme/Preferences.kt
+++ app/src/main/java/com/example/play_juegos/ui/theme/Preferences.kt
@@ -3,16 +3,24 @@ package com.example.play_juegos.ui.theme
 import android.preference.Preference
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -32,6 +40,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.play_juegos.New_player
+import kotlin.math.ceil
+import kotlin.math.floor
 
 @Composable
 fun Preferences() {
@@ -41,6 +51,8 @@ fun Preferences() {
     val range = 0.0f..100.0f
     val steps = 11
     var selection by remember { mutableStateOf(50f) }
+    var starsSelected by remember { mutableStateOf(0.0) }
+    var selectedChip by remember { mutableStateOf("No has seleccionado la plataforma") }
 
     Box(
         modifier = Modifier
@@ -66,9 +78,12 @@ fun Preferences() {
                     colors = SliderDefaults.colors(
                         thumbColor = Color(0xFF379665),
                         activeTrackColor = Color(0xFF379665),
-                        inactiveTrackColor = Color(0xFF379665)
+                        inactiveTrackColor = Color(0xFFACFBBE)
                     )
                 )
+                RatingBar(rating = starsSelected, stars = 10, onItemSelected = { newValue -> starsSelected = newValue })
+
+                MyFilterChip(selectedChip) {selectedChip = it}
             }
 
 
@@ -78,13 +93,42 @@ fun Preferences() {
         }
 
         FloatingActionButton(
-            onClick = { Toast.makeText(context, "Has seleccionado $stateOption con una puntuación de $selection", Toast.LENGTH_LONG).show() },
+            onClick = { Toast.makeText(context, "Has seleccionado $stateOption con una puntuación de0 $selection", Toast.LENGTH_LONG).show() },
             shape = CircleShape,
             modifier = Modifier
                 .padding(16.dp)
                 .align(alignment = Alignment.BottomEnd)
         ) {
             Icon(Icons.Default.Add, contentDescription = "Añadir")
+        }
+    }
+}
+
+
+@Composable
+fun MyFilterChip(selectedChip: String, onItemSelected: (String) -> Unit) {
+    val nombres = listOf("PS4", "XBOX", "3DS", "WII", "WIIU")
+    val context = LocalContext.current
+
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        nombres.forEach { name ->
+            FilterChip(
+                selected = selectedChip == name,
+                onClick = {
+                    onItemSelected(name)
+                    Toast.makeText(context, "Has seleccionado $name", Toast.LENGTH_LONG).show()
+                },
+                label = { Text(text = name) },
+                leadingIcon = if (selectedChip == name) {
+                    {
+                        Icon(
+                            imageVector = Icons.Filled.Done,
+                            contentDescription = "Done icon",
+                            modifier = Modifier.size(FilterChipDefaults.IconSize)
+                        )
+                    }
+                } else null
+            )
         }
     }
 }
@@ -109,6 +153,33 @@ fun MyRadioButton(stateOption: String, onItemSelected:(String) -> Unit) {
         }
     }
 
+}
+
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Double = 0.0,
+    stars: Int = 5,
+    starsColor: Color = Color(0xFF888888),
+    onItemSelected: (Double) -> Unit
+) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        for (i in 1..stars) {
+            val icon = if (i <= floor(rating).toInt()) {
+                Icons.Filled.Star
+            } else {
+                Icons.Outlined.Star
+            }
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = if (i <= rating) Color(0xFF379665) else starsColor,
+                modifier = Modifier
+                    .clickable { onItemSelected(i.toDouble()) }
+                    .padding(2.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
